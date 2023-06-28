c16e5e7d27960d1c7fe4aa585e8faf2a6c0ce24a
Elon Foster <121339825+elon900@users.noreply.github.com>
2023-06-28 15:16:48 +0530

    Update figlet.js

--- figlet.js
+++ figlet.js
@@ -1,9 +1,1 @@
-async function generateASCII() {
-  const inputText = document.getElementById('inputText').value;
-  const response = await fetch('https://hello-world-rough-river-c66d.elonfoster900.workers.dev/', {
-    method: 'POST',
-    body: inputText,
-  });
-  const asciiText = await response.text();
-  document.getElementById('output').innerText = asciiText;
-}
+
